c09fbfb50f66b90f23bf9ac717a863767a2af76b
Noman-Nom <noman5456318@gmail.com>
2023-12-08 00:50:02 +0500

    geting input data succesfuly

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 const getInputData = (inputData)=>{
 
     const inputValues = document.getElementById(inputData)
+    const values = []
+    // console.log("input Data = " , inputValues)
+    // console.log("input Data = " , inputValues.rows.length)
+   
 
-    console.log(inputValues)
+    for (let i = 0; i < inputValues.rows.length; i++) {
+        const input = inputValues.rows[i].cells[0].querySelector('input');
+    
+        values[i] = parseFloat(input.value)
+      
+    }
+    
+    console.log(values)
+    }
 
-}
+
+
 const calculate = ()=>{
 
     getInputData('inputData')
